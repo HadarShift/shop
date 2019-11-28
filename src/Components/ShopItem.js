94e762cd2ac7153cbed37f196471f62cb1f1c3a0
HadarShift <57561371+HadarShift@users.noreply.github.com>
2019-11-28 19:45:18 +0200

    start

---
+++ src/Components/ShopItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class ShopItem extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default ShopItem
